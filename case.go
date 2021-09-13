f7976778490edc39c447d8df631f1c00fbf7648d
Alex Studer <alex@studer.dev>
2021-09-12 23:48:08 -0400

    expand list of stopwords

--- case.go
+++ case.go
@@ -6,9 +6,27 @@ import (
 )
 
 var stopwords = map[string]bool{
-	"a":   true,
-	"an":  true,
-	"the": true,
+	"a":    true,
+	"an":   true,
+	"and":  true,
+	"as":   true,
+	"but":  true,
+	"for":  true,
+	"from": true,
+	"in":   true,
+	"into": true,
+	"like": true,
+	"nor":  true,
+	"of":   true,
+	"on":   true,
+	"or":   true,
+	"over": true,
+	"so":   true,
+	"the":  true,
+	"to":   true,
+	"upon": true,
+	"with": true,
+	"yet":  true,
 }
 
 func titleWord(word string) string {

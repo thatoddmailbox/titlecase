d5880b15c0042b09841e6f987be1bd923256dc61
Alex Studer <alex@studer.dev>
2021-09-12 23:22:36 -0400

    start work on unit tests

---
+++ case_test.go
@@ -0,0 +1,23 @@
+package titlecase_test
+
+import (
+	"testing"
+
+	"github.com/thatoddmailbox/titlecase"
+)
+
+func individualTest(t *testing.T, input string, expected string) {
+	result := titlecase.Title(input)
+	if result != expected {
+		t.Errorf(
+			"For input '%s': expected '%s', got '%s'",
+			input,
+			expected,
+			result,
+		)
+	}
+}
+
+func TestBasic(t *testing.T) {
+	individualTest(t, "word", "Word")
+}

9c9a93218b7163ecd0414e132654bccb03772106
Alex Studer <alex@studer.dev>
2021-09-12 23:49:04 -0400

    add some more test cases

--- case_test.go
+++ case_test.go
@@ -23,4 +23,13 @@ func TestBasic(t *testing.T) {
 	individualTest(t, "WORD", "Word")
 	individualTest(t, "interesting phrase", "Interesting Phrase")
 	individualTest(t, "testing yay", "Testing Yay")
+
+	individualTest(t, "This is a test", "This Is a Test")
+	individualTest(t, "give up the ghost", "Give Up the Ghost")
+	individualTest(t, "time out of mind", "Time Out of Mind")
+
+	// this one doesn't work because phrasal verbs are not handled correctly
+	// individualTest(t, "puttin' on the ritz", "Puttin' On the Ritz")
+
+	individualTest(t, "INTRO TO PSYCHOLOGY", "Intro to Psychology")
 }

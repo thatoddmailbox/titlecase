c2e521fc68cc82da529297a1543f429447566088
Alex Studer <alex@studer.dev>
2021-09-12 23:34:31 -0400

    parse each word individually

--- case.go
+++ case.go
@@ -1,8 +1,34 @@
 package titlecase
 
-import "strings"
+import (
+	"strings"
+	"unicode"
+)
 
 // Title converts the input string into title case.
 func Title(s string) string {
+	result := ""
+	word := ""
+	for _, nextRune := range s {
+		if unicode.IsSpace(nextRune) {
+			// we hit a separator, we have a complete word
+			// decide what to do to the word and append it
+			result += strings.Title(word)
+
+			// append this separator to the result
+			result += string(nextRune)
+
+			// reset the word
+			word = ""
+
+			continue
+		}
+
+		word += string(nextRune)
+	}
+	if len(word) != 0 {
+		result += strings.Title(word)
+	}
+
 	return strings.Title(s)
 }
